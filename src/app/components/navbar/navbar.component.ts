8ca77c6ecb141909fa8cead95fcd2950d3033e71
OnsLucerd <lucdictus161@gmail.com>
2023-03-30 23:36:04 +0200

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,17 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  const navbar = document.querySelector(".navbar") as HTMLElement;
+  @ViewChild('navbar', { static: true })
+  navbarRef!: ElementRef<HTMLElement>;
 
-  // Add a scroll event listener to the window object
-  window.addEventListener("scroll", () => {
-    // Check if the user has scrolled down at least 1 pixel
-    if (window.scrollY > 0) {
-      // Add a CSS class to the navbar to change its background color
-      navbar.classList.add("scrolled");
-    } else {
-      // Remove the CSS class if the user has scrolled back to the top
-      navbar.classList.remove("scrolled");
-    }
-  });
+  ngOnInit() {
+    // Add a scroll event listener to the window object
+    window.addEventListener("scroll", () => {
+      // Check if the user has scrolled down at least 1 pixel
+      if (window.scrollY > 0) {
+        // Add a CSS class to the navbar to change its background color
+        this.navbarRef.nativeElement.classList.add("scrolled");
+      } else {
+        // Remove the CSS class if the user has scrolled back to the top
+        this.navbarRef.nativeElement.classList.remove("scrolled");
+      }
+    });
+  }
 }
