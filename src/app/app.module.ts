c198e59d8aa858298531cf4c0247be711407c60b
Toby142 <tobiasvandeven142@gmail.com>
2023-04-05 13:45:22 +0200

    Revert "added pages and routing"
    This reverts commit 917de82e6fde315e08e8c38d4258dfe655abacb9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
-import { PaintersComponent } from './pages/painters/painters.component';
-import { ArtwordsComponent } from './pages/artwords/artwords.component';
-import { ArtworksComponent } from './pages/artworks/artworks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    PaintersComponent,
-    ArtwordsComponent,
-    ArtworksComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
